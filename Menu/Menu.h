cdc5e2a94393a09bfa84deaf22a28291cf774927
Vinicin <vincinbrbr@gmail.com>
2023-05-20 18:23:21 -0300

    testes no menu, alguns ajustes precisam ser realizados

--- Menu/Menu.h
+++ Menu/Menu.h
@@ -8,14 +8,23 @@ struct Menu{
 };
 
 void cls(){
-    system ("pause");
-    system ("cls");
+    system("pause");
+    system("cls");
 }
 
-void Menu::run() {
-    int opcao1, opcao2;
 
-    while(true) {
+
+void Menu::run() {
+    bool menu = true;
+    int opcao1 = 0, opcao2 = 0;
+    string nome;
+
+    Controlador controlador = Controlador();
+    Autor *autor = new Autor[10];
+
+
+
+    while(menu) {
         cout << "Digite a opção desejada!\n";
         cout << "\t[1] - Autor"<<endl;
         cout << "\t[2] - Editora"<<endl;
@@ -44,23 +53,32 @@ void Menu::run() {
                 cout << "\t[1] - Adicionar Autor"<<endl;
                 cout << "\t[2] - Exibir Autor"<<endl;
                 cout << "\t[3] - Remover Autor"<<endl;
-                cin<<opcao2;
+                cin >> opcao2;
                 cls();
 
-                while(opcao1 < 1 || opcao1 > 3) {
+                while(opcao2 < 1 || opcao2 > 3) {
                     cout << "Digito invalido, insira novamente!\n";
                     cout << "\t[1] - Adicionar Autor"<<endl;
                     cout << "\t[2] - Exibir Autor"<<endl;
                     cout << "\t[3] - Remover Autor"<<endl;
-                    cin<<opcao2;
+                    cin >> opcao2;
                     cls();
                 }
                 switch(opcao2){
-                    case 1: //<---------- Adicionar autor
+                    case 1:
+                        cout << "Diga o Nome do autor: "<< endl;
+                        cin >> nome;
+                        autor[0].nome = nome;
+                        controlador.adicionarAutor(autor[0]);
+                        break;
+                    case 2:
+                        controlador.mostrarAutor(autor[0].mat);
+                        controlador.mostrarAutor(autor[1].mat);
+                        break;
 
-                    case 2: //<---------- Exibir autor
-
-                    case 2:  //<---------- Remover autor
+                    /*case 3:  //<---------- Remover autor
+                        break;
+                    */
                 }
 
             case 2:
@@ -68,23 +86,26 @@ void Menu::run() {
                 cout << "\t[1] - Adicionar Editora"<<endl;
                 cout << "\t[2] - Exibir Editora"<<endl;
                 cout << "\t[3] - Remover Editora"<<endl;
-                cin<<opcao2;
+                cin >> opcao2;
                 cls();
 
-                while(opcao1 < 1 || opcao1 > 3) {
+                while(opcao2 < 1 || opcao2 > 3) {
                     cout << "Digito invalido, insira novamente!\n";
                     cout << "\t[1] - Adicionar Editora"<<endl;
                     cout << "\t[2] - Exibir Editora"<<endl;
                     cout << "\t[3] - Remover Editora"<<endl;
-                    cin<<opcao2;
+                    cin >> opcao2;
                     cls();
                 }
                 switch(opcao2){
                     case 1: //<---------- Adicionar Editora
+                        break;
 
                     case 2: //<---------- Exibir Editora
+                        break;
 
-                    case 2:  //<---------- Remover Editora
+                    case 3:  //<---------- Remover Editora
+                        break;
                 }
 
             case 3:
@@ -92,23 +113,26 @@ void Menu::run() {
                 cout << "\t[1] - Adicionar Livro"<<endl;
                 cout << "\t[2] - Exibir Livro"<<endl;
                 cout << "\t[3] - Remover Livro"<<endl;
-                cin<<opcao2;
+                cin >> opcao2;
                 cls();
 
-                while(opcao1 < 1 || opcao1 > 3) {
+                while(opcao2 < 1 || opcao2 > 3) {
                     cout << "Digito invalido, insira novamente!\n";
                     cout << "\t[1] - Adicionar Livro"<<endl;
                     cout << "\t[2] - Exibir Livro"<<endl;
                     cout << "\t[3] - Remover Livro"<<endl;
-                    cin<<opcao2;
+                    cin >> opcao2;
                     cls();
                 }
                 switch(opcao2){
                     case 1: //<---------- Adicionar Livro
+                        break;
 
                     case 2: //<---------- Exibir Livro
+                        break;
 
-                    case 2:  //<---------- Remover Livro
+                    case 3:  //<---------- Remover Livro
+                        break;
                 }
 
             case 4:
@@ -116,23 +140,26 @@ void Menu::run() {
                 cout << "\t[1] - Adicionar Revista"<<endl;
                 cout << "\t[2] - Exibir Revista"<<endl;
                 cout << "\t[3] - Remover Revista"<<endl;
-                cin<<opcao2;
+                cin >> opcao2;
                 cls();
 
-                while(opcao1 < 1 || opcao1 > 3) {
+                while(opcao2 < 1 || opcao2 > 3) {
                     cout << "Digito invalido, insira novamente!\n";
                     cout << "\t[1] - Adicionar Revista"<<endl;
                     cout << "\t[2] - Exibir Revista"<<endl;
                     cout << "\t[3] - Remover Revista"<<endl;
-                    cin<<opcao2;
+                    cin >> opcao2;
                     cls();
                 }
                 switch(opcao2){
                     case 1: //<---------- Adicionar Revista
+                        break;
 
                     case 2: //<---------- Exibir Revista
+                        break;
 
-                    case 2:  //<---------- Remover Revista
+                    case 3:  //<---------- Remover Revista
+                        break;
                 }
 
             case 5:
@@ -140,56 +167,32 @@ void Menu::run() {
                 cout << "\t[1] - Adicionar Usuario"<<endl;
                 cout << "\t[2] - Exibir Usuario"<<endl;
                 cout << "\t[3] - Remover Usuario"<<endl;
-                cin<<opcao2;
+                cin >> opcao2;
                 cls();
 
-                while(opcao1 < 1 || opcao1 > 3) {
+                while(opcao2 < 1 || opcao2 > 3) {
                     cout << "Digito invalido, insira novamente!\n";
                     cout << "\t[1] - Adicionar Usuario"<<endl;
                     cout << "\t[2] - Exibir Usuario"<<endl;
                     cout << "\t[3] - Remover Usuario"<<endl;
-                    cin<<opcao2;
+                    cin >> opcao2;
                     cls();
                 }
                 switch(opcao2){
                     case 1: //<---------- Adicionar Usuario
+                        break;
 
                     case 2: //<---------- Exibir Usuario
-
-                    case 2:  //<---------- Remover Usuario
+                        break;
+                    case 3:  //<---------- Remover Usuario
+                        break;
                 }
 
             case 6:
+                menu = false;
                 break;
                 }
         }
-
-
-        Controlador controlador = Controlador();
-
-        Autor author1 = Autor("Author 1");
-        Autor author2 = Autor("Author 2");
-
-        Editora publisher1 = Editora("Publisher 1");
-        Editora publisher2 = Editora("Publisher 2");
-
-        controlador.adicionarAutor(author1);
-        controlador.adicionarAutor(author2);
-
-        controlador.adicionarEditora(publisher1);
-        controlador.adicionarEditora(publisher2);
-
-        Livro book1 = Livro("Book teste", author1.mat, publisher1.mat, "2", false, "1");
-        Livro book2 = Livro("Book 2", author2.mat, publisher2.mat, "3", true, "2");
-
-        controlador.adicionarLivro(book1);
-        controlador.adicionarLivro(book2);
-
-        controlador.mostrarLivro(book1.mat);
-
-        controlador.mostrarAutor(author1.mat);
-        controlador.mostrarAutor(author2.mat);
-    }
 }
 
 #endif //TRABESTRUTURAM2_MENU_H
