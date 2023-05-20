50130fe505a4a5efd0750a34c465dbf76ba90ebc
Alisson <alissonpokrysilva@gmail.com>
2023-05-20 17:11:42 -0300

    Cabecalho do arquivo main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/*
+ * Universidade do Vale do Itajai - UNIVALI
+ * Prof. Marcos Cesar Cardoso Carrard
+ * Estrutura de Dados - Atividade de Implementação 2
+ *
+ *Alunos:   -Alisson Pokrywiecki da Silva
+ *          -Vinicius Setti
+ */
+
 #include <iostream>
 
 using namespace std;
