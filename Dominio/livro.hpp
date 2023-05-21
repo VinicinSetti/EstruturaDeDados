dbade5a3dd829658d739a7261548e6db4f280f06
Vinicin <vincinbrbr@gmail.com>
2023-05-21 10:43:32 -0300

    Dominio comentado

--- Dominio/livro.hpp
+++ Dominio/livro.hpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-struct Livro{
+struct Livro{ //struct Livro, foram declarados os atributos necessarios e algumas funcoes para funcionamento do codigo
 
     static int proxMat;
     int mat;
@@ -29,9 +29,9 @@ struct Livro{
 
 };
 
-int Livro::proxMat = 0;
+int Livro::proxMat = 0; //inicializacao da variavel, a fim de nao pegar lixo de memoria
 
-Livro::Livro() {
+Livro::Livro() { //metodo contrutor basico de livro
     this->mat = proxMat++;
     this->titulo = "";
     this->autor = 0;
@@ -42,7 +42,7 @@ Livro::Livro() {
     this->dataDoAluguel = "";
 }
 
-Livro::Livro(string titulo, int autor, int editora, string genero, bool alugado, string dataDoAluguel) {
+Livro::Livro(string titulo, int autor, int editora, string genero, bool alugado, string dataDoAluguel) { //Sobrecarga no meu metodo construtor de Livro, para salvamento das informacoes de seu cadastro
     this->mat = proxMat++;
     this->titulo = titulo;
     this->autor = autor;
@@ -53,23 +53,23 @@ Livro::Livro(string titulo, int autor, int editora, string genero, bool alugado,
     this->dataDoAluguel = dataDoAluguel;
 }
 
-void Livro::definirEmprestimo(bool alugado) {
+void Livro::definirEmprestimo(bool alugado) { //irá definor o atributo verificador se este livro foi pego empresatado ou nao
     this->alugado = alugado;
 }
 
-void Livro::dataRealizadaDoEmprestimo(string dataDoAluguel) {
+void Livro::dataRealizadaDoEmprestimo(string dataDoAluguel) { //Irá salvar a data em que foi emprestado o livro
     this->dataDoAluguel = dataDoAluguel;
 }
 
-void Livro::definirAutor(int autor) {
+void Livro::definirAutor(int autor) { //Irá salvar o autor do livro
     this->autor = autor;
 }
 
-void Livro::definirEditora(int editora) {
+void Livro::definirEditora(int editora) { //Irá salvar a editora do livro
     this->editora = editora;
 }
 
-void Livro::definirGenero(std::string genero) {
+void Livro::definirGenero(std::string genero) { //irá salvar o gereno do livro
     this->genero = genero;
 }
 #endif //TRABESTRUTURAM2_LIVRO_H
--- Dominio/revista.hpp
+++ Dominio/revista.hpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-struct Revista{
+struct Revista{ //struct Revista, foram declarados os atributos necessarios e algumas funcoes para funcionamento do codigo
     static int proxMat;
     int mat;
     int autor;
@@ -26,9 +26,9 @@ struct Revista{
     void definirGenero(string genero);
 };
 
-int Revista::proxMat = 0;
+int Revista::proxMat = 0; //inicializacao da variavel, a fim de nao pegar lixo de memoria
 
-Revista::Revista() {
+Revista::Revista() { //metodo contrutor basico de revista
     this->mat = proxMat++;
     this->titulo = "";
     this->autor = 0;
@@ -39,7 +39,7 @@ Revista::Revista() {
     this->dataDoAluguel = "";
 }
 
-Revista::Revista(std::string titulo, int autor, int editora, std::string genero) {
+Revista::Revista(std::string titulo, int autor, int editora, std::string genero) { //Sobrecarga no meu metodo construtor de revista, para salvamento das informacoes de seu cadastro
     this->mat = proxMat++;
     this->titulo = titulo;
     this->autor = autor;
@@ -50,23 +50,23 @@ Revista::Revista(std::string titulo, int autor, int editora, std::string genero)
     this->dataDoAluguel = "";
 }
 
-void Revista::definirGenero(std::string genero) {
+void Revista::definirGenero(std::string genero) { //irá salvar o gereno do revista
     this->genero = genero;
 }
 
-void Revista::definirEditora(int editora) {
+void Revista::definirEditora(int editora) { //Irá salvar a editora do revista
     this->editora = editora;
 }
 
-void Revista::definirAutor(int autor) {
+void Revista::definirAutor(int autor) { //Irá salvar o autor do revista
     this->autor = autor;
 }
 
-void Revista::definirEmprestimo(bool esmprestar) {
+void Revista::definirEmprestimo(bool esmprestar) { //irá definor o atributo verificador se esta revista foi pega empresatada ou nao
     this->alugado = esmprestar;
 }
 
-void Revista::dataRealizadaDoEmprestimo(std::string dataDoEmprestimo) {
+void Revista::dataRealizadaDoEmprestimo(std::string dataDoEmprestimo) { //Irá salvar a data em que foi emprestada
     this->dataDoAluguel = dataDoEmprestimo;
 }
 #endif //TRABESTRUTURAM2_REVISTA_H
